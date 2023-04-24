bce287e79ecd218b893e8a653992e5fe0aeb3665
Sree Harsha Chimbili <saisreeharsha@Srees-MacBook-Air.local>
2023-04-23 22:02:43 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+function App() {
+  const [usersList, setusersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setusersList((prevUsersList) => {
+      return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()}
+      ];
+    });
+  }
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
